f3fa7d96063de3a50b123f35a4369c96d0fd7376
imodclub <70686000+imodclub@users.noreply.github.com>
2022-06-14 15:34:16 +0700

    Edit routes routes route and insert index file success

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Login from './components/Login'
 import HomeUser from './components/HomeUser'
 import firebase from './services/firebase';
 import ProfileUser from './components/ProfileUser';
+import Index from './components/Index';
 import UserContext from './dataContext/userContext';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 
 function App() {
@@ -19,10 +20,23 @@ function App() {
   }, [])
   
   return (
-     <UserContext.Provider value={user}>
-      {user ? <HomeUser user={user} /> : <Login />}
-      </UserContext.Provider>
-       );
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} excat></Route>
+          <Route
+            path="homeuser"
+            element={user ? <HomeUser user={user} /> : <Login />}
+          ></Route>
+          <Route path="profileuser" element={<ProfileUser />}></Route>
+          <Route path="Login" element={<Login />}></Route>
+        </Routes>
+
+        <UserContext.Provider value={user}>{}</UserContext.Provider>
+      </BrowserRouter>
+    </div>
+  );
+      
 }
 
 export default App;
---
+++ src/components/Index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>Hello index page</div>
+  )
+}
+
+export default index
